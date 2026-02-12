bbf04839afb0f47da8cfc2fce90b01fe4ad8b375
Gabriela Oliveira <gabriela.oliveira1031@gmail.com>
2026-02-12 14:42:49 +0000

    feat: mensagem de erro como span abaixo da caixa de input e nao como alerta

--- modules/utils/validations.js
+++ modules/utils/validations.js
@@ -1,12 +1,22 @@
+const erroDescricao = document.querySelector('.erro-descricao');
+const erroQuantidade = document.querySelector('.erro-quantidade');
+
 export function validarTransacao(descricao, valor) {
     //verif descricao
     if (descricao.trim() === '') {
-        alert('A descrição não pode estar vazia.');
+        erroDescricao.textContent = 'A descrição não pode estar vazia.';
         return false;
     }
-    if (valor === '' || valor <= 0) {
-        alert('O valor deve ser maior que zero.');
+    erroDescricao.textContent = '';
+    if (valor === '') {
+        erroQuantidade.textContent = 'O valor não pode estar vazio.';
         return false;
     }
+    else if (valor <= 0) {
+        erroQuantidade.textContent = 'O valor deve ser maior que zero.';
+        return false;
+    }
+
+    erroQuantidade.textContent = '';
     return true;
 }

7fa88d55467486bd7e06efd6d3d1523a9d37c2f1
Gabriela Oliveira <gabriela.oliveira1031@gmail.com>
2026-02-13 13:46:40 +0000

    feat: verificação de tamanho de input para descricao e valor

--- modules/utils/validations.js
+++ modules/utils/validations.js
@@ -7,6 +7,10 @@ export function validarTransacao(descricao, valor) {
         erroDescricao.textContent = 'A descrição não pode estar vazia.';
         return false;
     }
+    else if (descricao.length > 29) {
+        erroDescricao.textContent = 'A descrição deve conter no máximo 29 caracteres.';
+        return false;
+    }
     erroDescricao.textContent = '';
     if (valor === '') {
         erroQuantidade.textContent = 'O valor não pode estar vazio.';
@@ -16,7 +20,10 @@ export function validarTransacao(descricao, valor) {
         erroQuantidade.textContent = 'O valor deve ser maior que zero.';
         return false;
     }
-
+    else if (valor > 1000000000) {
+        erroQuantidade.textContent = 'O valor deve ser menor que 1 bilhão.';
+        return false;
+    }
     erroQuantidade.textContent = '';
     return true;
 }
